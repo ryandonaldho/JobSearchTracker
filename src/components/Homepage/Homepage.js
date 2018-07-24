0f32b67ee12205f50dd7683839422f98ccdd0b55
Ryan <ryandonaldho@gmail.com>
2018-07-23 23:00:50 -0700

    mapping through spreadsheet

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -7,38 +7,10 @@ class Homepage extends Component {
 		super(props);
 	}
 
-	getSpreadsheet(){
-		const params = {
-		// The ID of the spreadsheet to retrieve data from.
-        spreadsheetId: '1LOZWvG84gTeRE7rU0T0wEQFeFKXkvQgyzSIWTgPDBUg',  // TODO: Update placeholder value.
-
-        // The A1 notation of the values to retrieve.
-        range: 'A1:B100',  // TODO: Update placeholder value.
-
-        // How values should be represented in the output.
-        // The default render option is ValueRenderOption.FORMATTED_VALUE.
-        valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
-
-        // How dates, times, and durations should be represented in the output.
-        // This is ignored if value_render_option is
-        // FORMATTED_VALUE.
-        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-        dateTimeRenderOption: 'SERIAL_NUMBER',  // TODO: Update placeholder value.
-		}
-
-		window.gapi.client.sheets.spreadsheets.values.get(params)
-		.then(response =>{
-			console.log(response.result)
-		})
-		.catch(err => {
-			console.log(err);
-		});
-
-	}
-
 	signedOutPressed = (e) =>{
 		e.preventDefault();
 		handleSignOutClick();
+		// init gapi again?
 		handleClientLoad();
 		if (getSignInStatus() === false){
 			this.props.setLoggedIn(false);
@@ -47,11 +19,10 @@ class Homepage extends Component {
 
 
 	render(){
-		this.getSpreadsheet();
 		return(
 			<div>
 				Home
-				<Spreadsheet/>
+				<Spreadsheet SpreadsheetId={"1LOZWvG84gTeRE7rU0T0wEQFeFKXkvQgyzSIWTgPDBUg"} />
 			<button id="signout-button" onClick={this.signedOutPressed}>Sign out</button>
 			</div>
 		);
--- src/components/Homepage/Spreadsheet.js
+++ src/components/Homepage/Spreadsheet.js
@@ -4,12 +4,63 @@ class Spreadsheet extends Component {
 
 	constructor(props){
 		super(props);
+		this.state = {
+			list: [[]]
+		}
 	}
 
+	getSpreadsheet(){
+		const params = {
+		// The ID of the spreadsheet to retrieve data from.
+	    spreadsheetId: this.props.SpreadsheetId,  // TODO: Update placeholder value.
+
+	    // The A1 notation of the values to retrieve.
+	    range: 'A1:D',  // TODO: Update placeholder value.
+
+	    // How values should be represented in the output.
+	    // The default render option is ValueRenderOption.FORMATTED_VALUE.
+
+	    valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
+
+	    // How dates, times, and durations should be represented in the output.
+	    // This is ignored if value_render_option is
+	    // FORMATTED_VALUE.
+	    // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+	    dateTimeRenderOption: 'SERIAL_NUMBER',  // TODO: Update placeholder value.
+		}
+
+		window.gapi.client.sheets.spreadsheets.values.get(params)
+		.then(response =>{
+			console.log(response.result);
+			this.setState({
+				list : response.result.values
+			})
+		})
+		.catch(err => {
+			console.log(err);
+		});
+
+	}
+
+	componentDidMount(){
+		this.getSpreadsheet();
+
+	}
+
+
 	render(){
+		const {list} = this.state;
+		console.log(list);
+		console.log(list[50]);
+		let listItems = list.map((item, index) =>{
+			//console.log(item[0]);
+			return <li key={index}> {item[0]} ======== {item[1]} </li>
+		}
+		);
 		return (
 			<div>
-			Spreadsheet
+			Test
+			<ul> {listItems} </ul>
 			</div>
 		);
 	}
